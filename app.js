41d1f791aa1f15cfaa24232cd67e3b68e2544a33
HaeChan Kim <bluesun147@naver.com>
2022-05-07 11:09:42 +0900

    chat

--- app.js
+++ app.js
@@ -34,9 +34,45 @@ io.sockets.on('connection', (socket) => {
 
     socket.on('disconnect', () => { // 연결되어 있던 소켓과 접속 끊어지면 자동으로 실행 됨
         console.log('접속 종료');
-    })
-})
+    });
+});
+
+// message: 클라가 서버로 메시지 전송
+// update: 서버에서 받은 메시지 다른 클라에게 전송
+// connectUser: 새로운 유저 접속 서버에게 알림
+
+io.sockets.on('connection', (socket) => {
+    // 새로운 유저가 접속했을 경우 다른 소켓에도 알려줌
+    socket.on('newUser', (name) => {
+        console.log(`${name} just connects!!`);
+
+        socket.name = name; // 소켓에 이름 저장해두기
+
+        // 모든 소켓에 전송
+        io.sockets.emit('update', {type: 'connect', name: 'SERVER', message:`${name} just connects!!`})
+    });
+
+    // 전송한 메시지 받기
+    socket.on('message', (data) => {
+        data.name = socket.name; // 받은 데이터에 누가 보냈는지 이름 추가
+        console.log(data);
+
+        // 보낸 사람을 제외한 나머지 유저에게 메시지 전송
+        socket.broadcast.emit('update', data);
+    });
+
+    // 접속 종료
+    socket.on('disconnect', () => {
+        console.log(`${socket.name} just left!!`);
+
+        // 나가는 사람 제외한 나머지 유저에게 메시지 전송
+        socket.broadcast.emit('update', {type: 'disconnect', name: 'SERVER', message: socket.Socket.name + 'just left!!'});
+    });
+    // io.sockets.emit() : 본인 포함 모든 유저
+    // io.broadcast.emit() // 본인 제외한 나머지 모두
+    //
+});
 
 server.listen(3000, () => {
-    console.log('서버 실행 중');
+    console.log('서버 실행 중 ..');
 })
--- static/js/index.js
+++ static/js/index.js
@@ -2,8 +2,47 @@ const socket = io();
 
 // 접속 되었을 때 실행
 socket.on('connect', () => {
-    let input = document.getElementById('test');
-    input.value = '접속 됨';
+    // 이름 입력 받기
+    let name = prompt('welcome!!', '');
+    // 이름 빈칸인 경우
+    if (!name) {
+        name = 'anym';
+    };
+
+    // 서버에 새로운 유저 왔다고 알림
+    socket.emit('newUser', name);
+});
+
+// 서버로부터 데이터 받은 경우
+socket.on('update', (data) => {
+    console.log(`${data.name}: ${data.message}`);
+    
+    let chat = document.getElementById('chat'); // 채탕
+
+    // 지정한 html 요소를 만들어 반환
+    let message = document.createElement('div');
+    // give it some conetent
+    let node = document.createTextNode(`${data.name}: ${data.message}`);
+    let className = '';
+
+    // 타입에 따라 적용할 클래스를 다르게 지정
+    switch(data.type) {
+        case 'message':
+            className = 'other';
+            break;
+
+        case 'connect':
+            className = 'connect';
+            break;
+        
+        case 'disconnect':
+            className = 'disconnect';
+            break;
+    }
+
+    message.classList.add(className);
+    message.appendChild(node);
+    chat.appendChild(message);
 });
 
 // 전송 함수. 버튼을 클릭했을 떄 호출 됨.
@@ -16,7 +55,15 @@ const send = () => {
     // 가져왔으니 데이터 빈 칸으로 변경
     document.getElementById('test').value = '';
 
-    // 서버로 send 이벤트 전달 + 데이터와 함께
+    let chat = document.getElementById('chat');
+    let msg = document.createElement('div');
+    let node = document.createTextNode(message);
+
+    msg.classList.add('me');
+    msg.appendChild(node);
+    chat.appendChild(msg);
+
     // on은 수신, emit은 전송
-    socket.emit('send', {msg: message});
-}
+    // 서버로 message 이벤트 전달 + 데이터와 함께
+    socket.emit('message', {type: 'message', message: message});
+};
